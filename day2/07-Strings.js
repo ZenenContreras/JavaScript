5547faca290252a4a87b864bae76f72082a0ea52
Zenen Contreras <zenencontreras1@gmail.com>
2024-08-14 08:11:28 -0500

    ~Practica 3

---
+++ day2/07-Strings.js
@@ -0,0 +1,10 @@
+let myNombre = 'zenen'
+let myAge = '22'
+
+console.log( 'hola, mi nombre es ' + myNombre + ' y tengo ' + myAge +' años')
+
+//Longitud
+
+console.log(myAge.length)
+console.log(typeof myAge)
+
