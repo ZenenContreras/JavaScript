85962c12be6c04e78dc2b11b6abd3721a8a83a2b
Zenen Contreras <zenencontreras1@gmail.com>
2024-08-19 14:55:36 -0500

    ~Practica 6

--- day4/18-EjerciciosFunciones.js
+++ day4/18-EjerciciosFunciones.js
@@ -3,11 +3,59 @@
 
 // 1. Crea una función que reciba dos números y devuelva su suma
 
+function func(a = 0, b = 0){
+    console.log('La suma es:', a + b)
+}
+
+func(2,3)
+
 // 2. Crea una función que reciba un array de números y devuelva el mayor de ellos
 
+myArray = [1,8,3,4,6]
+num = 0
+
+myArray.forEach(function(value){
+    for(let i = 0; i < myArray.length; i++){
+        if(myArray[i] > num){
+            num = myArray[i]
+        }
+    }
+})
+
+console.log(`El numero mayor es: [${num}] `)
 // 3. Crea una función que reciba un string y devuelva el número de vocales que contiene
 
+string = 'Hola como estas'
+vocales1 = 'aeiouAEIOU'
+contador = 0
+
+function vocales(arr){
+    for(let i = 0; i < string.length;i++){
+        if(vocales1.includes(string[i])){
+            contador++
+        }
+    }
+    console.log(contador)
+    
+}
+
+console.log(vocales(string))
+
 // 4. Crea una función que reciba un array de strings y devuelva un nuevo array con las strings en mayúsculas
+
+let strings =['z','e','n','e','n']
+let mayusc = []
+
+function tomayusc(arr){
+
+    arr.forEach(function(value){
+        mayusc.push(value.toUpperCase())
+    })
+    return mayusc
+}
+
+
+console.log(tomayusc(strings))
 
 // 5. Crea una función que reciba un número y devuelva true si es primo, y false en caso contrario
 
