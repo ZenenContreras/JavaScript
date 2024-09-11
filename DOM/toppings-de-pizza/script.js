ad30e29e97d8da13c9b52e88ca2c96a69cc368c6
Zenen Contreras <zenencontreras1@gmail.com>
2024-09-11 11:04:23 -0500

    dom practica

---
+++ DOM/toppings-de-pizza/script.js
@@ -0,0 +1,7 @@
+const contenedor = document.getElementById('contenedor');
+const titulo = document.getElementById('titulo');
+
+console.log(titulo.innerText); 
+console.log(titulo.tagName); 
+console.log(titulo.innerHTML); 
+
