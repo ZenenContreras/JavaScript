2135026b6d98170b94132d522c92ac0468df7712
Zenen Contreras Royero <zenencontreras1@gmail.com>
2024-09-19 22:25:39 -0500

    Refactorizar clases de toppings en el archivo index.html y agregar selección de elementos en el archivo script.js

--- DOM/toppings-de-pizza/script.js
+++ DOM/toppings-de-pizza/script.js
@@ -1,3 +1,5 @@
+//Seleccionar todos los elementos por su id
+
 const contenedor = document.getElementById('contenedor');
 const titulo = document.getElementById('titulo');
 
@@ -5,3 +7,30 @@ console.log(titulo.innerText);
 console.log(titulo.tagName); 
 console.log(titulo.innerHTML); 
 
+//Elementos por su clase
+const topping = document.getElementsByClassName("toppings")
+
+console.log(topping) 
+
+
+//Se puede seleccionar todos los elementos por su tag
+const misToppings = document.getElementsByTagName("li")
+
+console.log(misToppings)
+
+//ejemplo
+
+const rojo = document.getElementsByClassName("rojo")
+
+console.log(rojo)
+
+//seleccionar un elemento por un selector css
+
+//Selecciona el primero que cumpla con el criterio
+
+const aceituna = document.querySelector("#aceitunas")
+const toppings = document.querySelector(".toppings")
+console.log(aceituna)
+console.log(toppings)
+
+//Selecciona todos que cumpla con el criterio
